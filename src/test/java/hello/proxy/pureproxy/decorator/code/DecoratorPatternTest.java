8dc85e458e53f433d0d46aa854842fef4d6a7c68
이경환 <khlee@rsupport.com>
2025-08-22 16:08:47 +0900

    feat: 데코레이터 페턴 - 데코레이터 패턴 적용 테스트코드 작성 (부가 기능 추가 - result 변형)

--- src/test/java/hello/proxy/pureproxy/decorator/code/DecoratorPatternTest.java
+++ src/test/java/hello/proxy/pureproxy/decorator/code/DecoratorPatternTest.java
@@ -1,5 +1,6 @@
 package hello.proxy.pureproxy.decorator.code;
 
+import hello.proxy.pureproxy.proxy.code.MessageDecorator;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -11,6 +12,14 @@ public class DecoratorPatternTest {
     Component realComponent = new RealComponent();
     DecoratorPatternClient client = new DecoratorPatternClient(realComponent);
 
+    client.execute();
+  }
+
+  @Test
+  void decorator1() {
+    Component realComponent = new RealComponent();
+    Component messageComponent = new MessageDecorator(realComponent);
+    DecoratorPatternClient client = new DecoratorPatternClient(messageComponent);
     client.execute();
   }
 
