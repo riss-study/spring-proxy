721bdb65b6d0aa41a06f5f9013cd63f9ea2c5215
이경환 <khlee@rsupport.com>
2025-12-29 15:46:45 +0900

    feat: 빈 후처리기 - 기본 예제

---
+++ src/test/java/hello/proxy/postprocessor/BasicTest.java
@@ -0,0 +1,48 @@
+package hello.proxy.postprocessor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BasicTest {
+
+  @Test
+  void basicConfig() {
+    ApplicationContext context = new AnnotationConfigApplicationContext(BasicConfig.class);
+
+    // A 빈으로 등록
+    ObjectA a = context.getBean("beanA", ObjectA.class);
+    a.hello();
+
+    // B 빈으로 등록 X
+    Assertions.assertThrows(NoSuchBeanDefinitionException.class, () -> context.getBean("beanB"));
+  }
+
+  @Slf4j
+  @Configuration
+  static class BasicConfig {
+    @Bean(name = "beanA")
+    public ObjectA a() {
+      return new ObjectA();
+    }
+  }
+
+  @Slf4j
+  static class ObjectA {
+    public void hello() {
+      log.info("hello A");
+    }
+  }
+
+  @Slf4j
+  static class ObjectB {
+    public void hello() {
+      log.info("hello B");
+    }
+  }
+}
